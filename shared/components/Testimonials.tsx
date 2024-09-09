2a54e3f9e5faf9f6abe68b25e564438d77f11d23
fudge-fantastic <siddhant.pandagle1998@gmail.com>
2024-09-10 01:25:13 +0530

    Bug fixes and changes

--- shared/components/Testimonials.tsx
+++ shared/components/Testimonials.tsx
@@ -12,79 +12,7 @@ export default function Testimonials() {
                 <section className="bg-white">
                     <div className="container px-6 mx-auto">
                         <section className="grid grid-cols-1 gap-8 xl:mt-12 lg:grid-cols-2 xl:grid-cols-2">
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “Great learning experience, Repeats the topic multiple times. Professors are industry experts so they provide insider knowledge and tricks pthat could only be learnt after working multiple years in the industry.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Ankit Kalekar</h1>
-                                            {/* <span className="text-sm text-gray-500">CTO, Robert Consultency</span> */}
-                                        </div>
-                                </div>
-                            </div>
-
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “Yogesh Singh excels as a data science instructor, standing out for his ability to simplify complex concepts by using real-life business scenarios. His teaching method, incorporating related examples, ensures that everyone can easily grasp the material. His expertise and engaging approach make him the go-to for understanding data science.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Atmaram Bansode</h1>
-                                            {/* <span className="text-sm text-gray-500">CEO, Jeny Consultency</span> */}
-                                        </div>
-                                </div>
-                            </div>
-
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “I am happy to share…that NSArrows stands on there words, 3 months back got my first internship and today i got a full time job as a business analyst wth a good package. Really thankful to Mr. Yogesh Singh, sir your training and guidance that has changed my life, was scared about the job market but through the corporate connect program, everything went pretty smooth. A big thank you.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Shravani Suryawanshi</h1>
-                                            {/* <span className="text-sm text-gray-500">Marketing Manager at Stech</span> */}
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                Hi, everyone I am happy to share my learning experience. I am learning Data Science. My  trainer was Yogesh Sir. His teaching style is very unique. He helps me a lot during learning phase. He explain every concepts very clearly and gave real life example, that’s why every complicated topic can be easily understand. #Mentor #Leader”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Abhishekh Pawar</h1>
-                                            {/* <span className="text-sm text-gray-500">CTO, Robert Consultency</span> */}
-                                        </div>
-                                </div>
-                            </div>
-
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “Yogesh is an amazing mentor and trainer. I enjoyed my Data Science learning journey with Yogesh as most of my cohort colleagues agree he has the talent to explain very complex concepts in a very simple way. He is generous, Yogesh always sheres with you his own experience of how real-life models are built and what outcome the industry is expecting from a Data Scientist. Thanks a lot Yogesh for all what you shared.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Youssef Senhaji Rahzi</h1>
-                                            {/* <span className="text-sm text-gray-500">CEO, Jeny Consultency</span> */}
-                                        </div>
-                                </div>
-                            </div>
+                            
 
                             <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
                                 <p className="leading-loose text-gray-500 text-left text-sm ">
@@ -130,20 +58,6 @@ export default function Testimonials() {
                             </div>
                             <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
                                 <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “Yogesh Singh Is such a nice person to have around. He is a great mentor with extensive knowledge and expertise on data science. Yogesh supervised my capstone project on house price prediction, and I learnt extremety working with him. He is creative, very hardworking and always willing to help.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Maria R Kilrain</h1>
-                                            <span className="text-sm text-gray-500">Data Scientist</span>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
                                     “Yogesh was an excellent Data Science mentor and concisely explained numerous learning models as well as bow to apply them to real work data, He went the extra mile for students working on their own projects and made sure to explain how they could deploy the models we learned for their own needs. Lastly, he kept things brief. Often instructors get bogged down in minimally important details that aren't relevant in real world applications. Yogesh made sure to inform us which aspects of our models were commonly deployed in real world settings as well as what details held little relevance. Thanks Yogesh!”
                                 </p>
 
@@ -153,21 +67,6 @@ export default function Testimonials() {
                                         <div className="mx-2 text-left">
                                             <h1 className="font-semibold text-gray-800 text-right ">Anatole Colevas</h1>
                                             <span className="text-sm text-gray-500">Date Analyst Database Management Pallys Modeling and Visualizalice</span>
-                                        </div>
-                                </div>
-                            </div>
-
-                            <div className="p-4 border rounded-lg flex flex-col justify-between shadow-md">
-                                <p className="leading-loose text-gray-500 text-left text-sm ">
-                                    “I am happy to share…that NSArrows stands on there words, 3 months back got my first internship and today i got a full time job as a business analyst wth a good package. Really thankful to Mr. Yogesh Singh, sir your training and guidance that has changed my life, was scared about the job market but through the corporate connect program, everything went pretty smooth. A big thank you.”
-                                </p>
-
-                                <div className="flex justify-end items-center -mx-2">
-                                    {/* <Image height={100} width={100} className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/> */}
-
-                                        <div className="mx-2 text-left">
-                                            <h1 className="font-semibold text-gray-800 text-right ">Ema Watson </h1>
-                                            <span className="text-sm text-gray-500">Marketing Manager at Stech</span>
                                         </div>
                                 </div>
                             </div>
